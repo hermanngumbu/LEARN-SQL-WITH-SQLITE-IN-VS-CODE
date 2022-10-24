98d5534764559988c5e981b808b30628a13cc5fd
hermanngumbu <https://github.com/hermanngumbu.com>
2022-10-24 16:26:38 +0100

    created db, table and insert data

---
+++ -- SQLite.sql
@@ -0,0 +1,18 @@
+-- SQLite
+INSERT INTO recipes 
+        (
+            title, 
+            slug, 
+            content, 
+            duration, 
+            online, 
+            created_at
+        )
+VALUES  ( 
+            'Salade de fruit',
+            'salade-de-fruit',
+            'Contenu du test',
+             30,
+             TRUE,
+            1666620231
+        );
